cd9b9acf0d07ab392c4564d2ebf76b776fe6dae6
Jami9100 <jamijasmitha9@gmail.com>
2026-03-27 15:38:47 +0530

    hello using multiple print statements

--- HelloApp.java
+++ HelloApp.java
@@ -2,27 +2,14 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-      
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } 
         else {
-    
-            StringBuilder nameBuilder = new StringBuilder();
 
-           
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
+            String names = String.join(", ", args);
 
-          
-            String names = nameBuilder.toString();
-
-            if (names.length() > 0) {
-                names = names.substring(0, names.length() - 2);
-            }
-
-          
+         
             System.out.println("Hello, " + names + "!");
         }
     }
